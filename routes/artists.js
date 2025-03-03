d5fde94b35726136e98d35a3737e6fc6f4818e22
elciguapuuu <sahirposas@gmail.com>
2025-03-03 11:16:04 +0100

    used prisma client in artists

--- routes/artists.js
+++ routes/artists.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const data = [];
+router.get('/', async function(req, res, next) {
+  const data = await prisma.artists.findMany();
   res.json(data);
 });
 
